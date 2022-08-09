8a925c9afb9c9e0140cd260080df31eabd4e840c
Serkankrgl <51543224+Serkankrgl@users.noreply.github.com>
2022-08-09 14:15:19 +0300

    Static files added.

--- PCAT/app.js
+++ PCAT/app.js
@@ -1,3 +1,18 @@
-const fn = function fn() {
-  console.log('test');
-};
+const express = require('express');
+const path = require('path');
+const app = express();
+//MIDDLEWARES
+
+// const myLogger = (req, res, next) => {
+//   console.log('middleware');
+//   next();
+// };
+
+app.use(express.static('public'));
+app.use(myLogger);
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {});
