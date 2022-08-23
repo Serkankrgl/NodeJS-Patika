267a23f3a94f1f36f8244f6e91bbae1d15a8f975
Serkankrgl <51543224+Serkankrgl@users.noreply.github.com>
2022-08-23 22:14:33 +0300

    Partials added.

--- EDU/app.js
+++ EDU/app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const ejs = require('ejs');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.status(200).send('INDEX');
+    res.status(200).render('index', {
+        page_name: 'index',
+    });
 });
-
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: 'about',
+    });
+});
 const port = 3000;
 app.listen(port, () => {
     console.log('Live!!');
